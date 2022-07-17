c7dc64673e0fc8a35fabe5a2aee678ff31ca0b1a
Nicolas Knaian <nickknaian@gmail.com>
2022-07-16 22:26:27 -0400

    poc for single video clip & picture

--- snippet_server/main/handlers.py
+++ snippet_server/main/handlers.py
@@ -1,10 +1,21 @@
 import os
-from random import choice
+import glob
+import random
+import shutil
 import pathlib
+from moviepy.editor import VideoFileClip
 
 from flask import render_template, url_for
 
 from . import bp
+
+
+"""CONSTANTS"""
+
+
+CLIP_LENGTH_SEC = 30
+MEDIA_DIR = 'snippet_server/static/media'
+USBDRIVE_DIR = 'usbdrive'  # temp
 
 
 """HANDLER FUNCTIONS"""
@@ -12,13 +23,75 @@ from . import bp
 
 @bp.route('/', methods=['GET', 'POST'])
 def index():
-    # Pick a random image from the image folder
-    image_names = [
-        file_name for file_name in os.listdir(os.path.join('snippet_server', 'static', 'images'))
-        if pathlib.Path(file_name).suffix == ".jpg"
-    ]
+    image_name = random_image()
+    video_name = random_video_clip()
 
     return render_template(
         'main/index.html',
-        image_name=choice(image_names) if len(image_names) else None
+        image_name=image_name,
+        video_name=video_name
     )
+
+
+"""Private Funtions"""
+
+
+def random_image():
+    """Refresh the random image in the media directory
+    
+    Returns the name of the file created in the media dir
+    """
+    # Remove any old movie phots in the media dir
+    for file in os.listdir(MEDIA_DIR):
+        if file.endswith(".jpg"):
+            os.remove(os.path.join(MEDIA_DIR, file))
+
+    # Get all image file choices
+    image_choices = []
+    for filename in glob.iglob('usbdrive/**/*.jpg', recursive=True):
+        image_choices.append(os.path.abspath(filename))
+
+    # Choose one of the phots
+    image_path = random.choice(image_choices)
+
+    # Copy the image to the media dir
+    shutil.copy(image_path, MEDIA_DIR)
+
+    return os.path.basename(image_path)
+
+
+def random_video_clip():
+    """Refresh the random video clip in the media directory
+    
+    Returns the name of the file created in the media dir
+    """
+    # Remove any old movie files in the media dir
+    for file in os.listdir(MEDIA_DIR):
+        if file.endswith(".MP4") or file.endswith(".mov"):
+            os.remove(os.path.join(MEDIA_DIR, file))
+
+    # Get all movie file choices
+    video_choices = []
+    for filename in glob.iglob(f'{USBDRIVE_DIR}/**/*.MP4', recursive=True):
+        video_choices.append(os.path.abspath(filename))
+
+    for filename in glob.iglob(f'{USBDRIVE_DIR}/**/*.mov', recursive=True):
+        video_choices.append(os.path.abspath(filename))
+
+    # Choose one of the videos
+    video_path = random.choice(video_choices)
+
+    # Copy a 30 second (or less) clip from the chosen video to the media folder
+    video_clip = VideoFileClip(video_path)
+    video_clip_name = ""
+    if video_clip.duration <= CLIP_LENGTH_SEC:
+        shutil.copy(video_path, MEDIA_DIR)
+        video_clip_name = os.path.basename(video_path)
+    else:
+        start = random.randint(0, int(video_clip.duration) - CLIP_LENGTH_SEC)
+        end = start + CLIP_LENGTH_SEC
+        clip = video_clip.subclip(start, end)
+        video_clip_name = f"{pathlib.Path(video_path).stem}_{start}_{end}.MP4"
+        clip.write_videofile(os.path.join(MEDIA_DIR, video_clip_name))
+
+    return video_clip_name
---
+++ snippet_server/static/media/README.md
@@ -0,0 +1,1 @@
+Images and videos being served will be stored here
